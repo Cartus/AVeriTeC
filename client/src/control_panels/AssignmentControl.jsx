422d4378374843422192f3eae2cfb55c50bfd8e5
Michael <michael.sejr@gmail.com>
2021-10-14 15:19:33 +0100

    control site updates

--- client/src/control_panels/AssignmentControl.jsx
+++ client/src/control_panels/AssignmentControl.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
 
 const EntryCard = styled(Card)`
     margin:10px;
@@ -8,6 +9,12 @@ const EntryCard = styled(Card)`
 `
 
 const Header = styled.h4`
+`
+
+const StartButton = styled(Button)`
+float:left;
+width:120px;
+margin:10px 0px!important;
 `
 
 
@@ -28,13 +35,26 @@ class AssignmentControl extends React.Component {
             return <li><a href={link_str}>{assignment}</a></li>
         });
 
+        if (this.props.assignments.length > 0){
+            var firstAssignmentLink = "/" + this.props.page + "?claim_id=" + this.props.assignments[0]
+        }
+
         return (
             <div className={className}>
                 <EntryCard>
                     <Header>Assignments: {this.props.name}</Header>
                     <ul>
                         {assignments}
-                    </ul>
+                    </ul>         
+                    {(this.props.assignments.length > 0)?                
+                    <StartButton variant="contained" color="primary" onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href=firstAssignmentLink;
+                        }}>
+                        Start Next
+                    </StartButton>
+                    : ""
+                    }
                 </EntryCard> 
             </div>
         );
