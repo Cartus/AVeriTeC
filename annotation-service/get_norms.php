482b6cca6ff9cff0b3402e6cbd51107d910a16d8
Cartus <cartusguo@gmail.com>
2022-07-11 14:31:26 +0100

    updated get_p1_norms

--- annotation-service/get_norms.php
+++ annotation-service/get_norms.php
@@ -12,37 +12,54 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM Norm_Claims";
 $result = $conn->query($sql);
 
+$gold_array = array();
+
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "claim_norm_id: " . $row["claim_norm_id"]. "<br>";
-        echo "claim_id: " . $row["claim_id"]. "<br>";
-        echo "web_archive: " . $row["web_archive"]. "<br>";
-        echo "user_id_norm: " . $row["user_id_norm"]. "<br>";
-        echo "cleaned_claim: " . $row["cleaned_claim"]. "<br>";
-        echo "correction_claim: " . $row["correction_claim"]. "<br>";
-        echo "speaker: " . $row["speaker"]. "<br>";
-        echo "hyperlink: " . $row["hyperlink"]. "<br>";
-        echo "source: " . $row["source"]. "<br>";
-        echo "transcription: " . $row["transcription"]. "<br>";
-        echo "media_source: " . $row["media_source"]. "<br>";
-        echo "check_date: " . $row["check_date"]. "<br>";
-        echo "claim_loc: " . $row["claim_loc"]. "<br>";
-        echo "claim_types: " . $row["claim_types"]. "<br>";
-        echo "fact_checker_strategy: " . $row["fact_checker_strategy"]. "<br>";
-        echo "phase_1_label: " . $row["phase_1_label"]. "<br>";
-        echo "latest: " . $row["latest"]. "<br>";
-        echo "date_start_norm: " . $row["date_start_norm"]. "<br>";
-        echo "date_load_norm: " . $row["date_load_norm"]. "<br>";
-        echo "date_made_norm: " . $row["date_made_norm"]. "<br>";
-        echo "date_restart_norm: " . $row["date_restart_norm"]. "<br>";
-        echo "date_modified_norm: " . $row["date_modified_norm"]. "<br>";
-        echo "nonfactual: " . $row["nonfactual"]. "<br>";
-        echo "inserted: " . $row["inserted"]. "<br>";
-        echo "<br>";
+        // echo "claim_norm_id: " . $row["claim_norm_id"]. "<br>";
+        // echo "claim_id: " . $row["claim_id"]. "<br>";
+        // echo "web_archive: " . $row["web_archive"]. "<br>";
+        // echo "user_id_norm: " . $row["user_id_norm"]. "<br>";
+        // echo "cleaned_claim: " . $row["cleaned_claim"]. "<br>";
+        // echo "correction_claim: " . $row["correction_claim"]. "<br>";
+        // echo "speaker: " . $row["speaker"]. "<br>";
+        // echo "hyperlink: " . $row["hyperlink"]. "<br>";
+        // echo "source: " . $row["source"]. "<br>";
+        // echo "transcription: " . $row["transcription"]. "<br>";
+        // echo "media_source: " . $row["media_source"]. "<br>";
+        // echo "check_date: " . $row["check_date"]. "<br>";
+        // echo "claim_loc: " . $row["claim_loc"]. "<br>";
+        // echo "claim_types: " . $row["claim_types"]. "<br>";
+        // echo "fact_checker_strategy: " . $row["fact_checker_strategy"]. "<br>";
+        // echo "phase_1_label: " . $row["phase_1_label"]. "<br>";
+        // echo "latest: " . $row["latest"]. "<br>";
+        // echo "date_start_norm: " . $row["date_start_norm"]. "<br>";
+        // echo "date_load_norm: " . $row["date_load_norm"]. "<br>";
+        // echo "date_made_norm: " . $row["date_made_norm"]. "<br>";
+        // echo "date_restart_norm: " . $row["date_restart_norm"]. "<br>";
+        // echo "date_modified_norm: " . $row["date_modified_norm"]. "<br>";
+        // echo "nonfactual: " . $row["nonfactual"]. "<br>";
+        // echo "inserted: " . $row["inserted"]. "<br>";
+        // echo "<br>";
+
+        array_push($gold_array, ["claim_norm_id"=> $row['claim_norm_id'], "claim_id"=> $row['claim_id'], "web_archive"=> $row['web_archive'], "user_id_norm"=> $row['user_id_norm'],
+        "cleaned_claim"=> $row['cleaned_claim'], "correction_claim"=> $row['correction_claim'], "speaker"=> $row['speaker'], "hyperlink"=> $row['hyperlink'], "source"=> $row['source'],
+        "transcription"=> $row['transcription'], "media_source"=> $row['media_source'], "check_date"=> $row['check_date'], "claim_loc"=> $row['claim_loc'], "claim_types"=> $row['claim_types'],
+        "fact_checker_strategy"=> $row['fact_checker_strategy'], "phase_1_label"=> $row['phase_1_label'], "latest"=> $row['latest'], "date_start_norm"=> $row['date_start_norm'], ""=> $row[''],
+        "date_load_norm"=> $row['date_load_norm'], "date_made_norm"=> $row['date_made_norm'], "date_restart_norm"=> $row['date_restart_norm'], "date_modified_norm"=> $row['date_modified_norm'],
+        "nonfactual"=> $row['nonfactual'], "inserted"=> $row['inserted']])
     }
 } else {
     echo "0 Results";
 }
+
+$json = json_encode($gold_array);
+
+//write json to file
+if (file_put_contents("results/p1_norms.json", $json))
+    echo "JSON file created successfully...";
+else
+    echo "Oops! Error creating json file...";
 
 $conn->close();
 ?>
--- annotation-service/update_annotators.php
+++ annotation-service/update_annotators.php
@@ -18,9 +18,9 @@ if ($conn->connect_error) {
 }
 
 
-$id = 8;
-$pd = 'user131';
-$pd_md5='d0732364726e6527d17965baa1973f1f';
+// $id = 8;
+// $pd = 'user131';
+// $pd_md5='d0732364726e6527d17965baa1973f1f';
 
 // $sql = 'UPDATE Annotators SET password_md5="599b60f343e0cd5d7623c0e586b44ab6" WHERE user_id=42';
 // update_table($conn, "UPDATE Annotators SET password_cleartext=? AND password_md5=? WHERE user_id=?", 'ssi', $pd, $pd_md5, $id);
@@ -29,7 +29,7 @@ $pd_md5='d0732364726e6527d17965baa1973f1f';
 
 // update_table($conn, "UPDATE Annotators SET p5_assigned=0, finished_post_annotations=0 WHERE user_id=?", 'i', $id);
 
-$sql = 'UPDATE Annotators SET current_norm_task=NULL, finished_norm_annotations=0, skipped_norm_data=0, p1_time_sum=0, p1_load_sum=0, p1_speed_trap=0, p1_timed_out=0, p1_assigned=0 WHERE user_id=42';
+$sql = 'UPDATE Annotators SET current_norm_task=NULL, finished_norm_annotations=0, skipped_norm_data=0, p1_time_sum=0, p1_load_sum=0, p1_speed_trap=0, p1_timed_out=0, p1_assigned=0 WHERE user_id=8';
 
 if ($conn->query($sql) === TRUE) {
     echo "Table Annotators updated successfully";
