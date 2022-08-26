52e69c8d3cb7f5b8d7b3fc05776b3d1602c95a14
Michael Schlichtkrull <michael.sejr@gmail.com>
2022-08-26 12:25:39 +0100

    Added report media claim button

--- client/src/averitec_components/ReportBar.jsx
+++ client/src/averitec_components/ReportBar.jsx
@@ -39,10 +39,10 @@ class ReportBar extends React.Component {
     this.onReport = this.onReport.bind(this)
   }
 
-  async onReport() {
+  async onReport(skip_reason) {
     var dataset = "annotation"
-    if (this.props.dataset){
-        dataset = this.props.dataset
+    if (this.props.dataset) {
+      dataset = this.props.dataset
     }
     console.log("I'm skipping for dataset: " + dataset)
 
@@ -58,17 +58,18 @@ class ReportBar extends React.Component {
           dataset: dataset,
           user_id: localStorage.getItem('user_id'),
           req_type: 'skip-data',
-          claim_id: localStorage.claim_id
+          claim_id: localStorage.claim_id,
+          skip_reason: skip_reason
         }
       };
 
       await axios(request).then((response) => {
         console.log(response.data);
-        if (dataset === "training"){
+        if (dataset === "training") {
           localStorage.train_finished_norm_annotations = Number(localStorage.train_finished_norm_annotations) + 1;
         } else {
           localStorage.finished_norm_annotations = Number(localStorage.finished_norm_annotations) + 1;
-        }  
+        }
         window.location.reload(false);
       }).catch((error) => { window.alert(error) })
     } else if (phase === 'phase_2') {
@@ -80,39 +81,59 @@ class ReportBar extends React.Component {
           dataset: dataset,
           user_id: localStorage.getItem('user_id'),
           req_type: 'skip-data',
-          claim_norm_id: localStorage.claim_norm_id
+          claim_norm_id: localStorage.claim_norm_id,
+          skip_reason: skip_reason
         }
       };
 
-      console.log("I'm skipping the claim with norm id "+localStorage.claim_norm_id)
+      console.log("I'm skipping the claim with norm id " + localStorage.claim_norm_id)
 
       await axios(request).then((response) => {
         console.log(response.data);
-        
-        if (dataset === "training"){
+
+        if (dataset === "training") {
           localStorage.train_finished_qa_annotations = Number(localStorage.train_finished_qa_annotations) + 1;
         } else {
           localStorage.finished_qa_annotations = Number(localStorage.finished_qa_annotations) + 1;
-        }    
-        
+        }
+
         window.location.reload(false);
       }).catch((error) => { window.alert(error) })
     }
   };
 
   render() {
+    let phase = localStorage.getItem('phase');
+    var extra_media_skipper = ""
+    if (phase === 'phase_2' || phase === 'phase_4') {
+      extra_media_skipper = <EntryCard>
+        <ReportDiv data-tour="report">
+          <ReportTextDiv>
+            If the claim <b>directly refers</b> to an image, video, or audio clip, and <b>cannot be understood</b> without viewing or hearing it, the claim is a media claim that erroneously passed Phase One. In that case, please use this button to report the claim to use and skip it. Please be aware you cannot report a claim if you have already submitted an annotation for it.
+          </ReportTextDiv>
+
+          <ReportButton variant="contained" disabled={Number(localStorage.pc) > 0} color="error" onClick={() => this.onReport("media")}>
+            Report Media Claim
+          </ReportButton>
+        </ReportDiv>
+      </EntryCard>
+    }
+
     return (
+      <div>
       <EntryCard>
         <ReportDiv data-tour="report">
           <ReportTextDiv>
             If the fact checking article displays an error, is behind a paywall, or if it takes more than three minutes to load, please let us know and skip the claim. Please be aware you cannot report a claim if you have already submitted an annotation for it.
           </ReportTextDiv>
 
-          <ReportButton variant="contained" disabled={Number(localStorage.pc) > 0} color="error" onClick={this.onReport}>
+          <ReportButton variant="contained" disabled={Number(localStorage.pc) > 0} color="error" onClick={() => this.onReport("other")}>
             Report &amp; Skip
           </ReportButton>
         </ReportDiv>
       </EntryCard>
+      {extra_media_skipper}
+      </div>
     );
   }
 }
